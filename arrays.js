e394f0ea4212bd13d397611d80bcb931dba77110
Alcee Metevier <almetevier@gmail.com>
2020-05-01 15:10:43 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,22 @@
+
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element) {
+  var newArr = [element, ...array]
+  return newArr;
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.unshift(element)
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array, element]
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array; 
+}
